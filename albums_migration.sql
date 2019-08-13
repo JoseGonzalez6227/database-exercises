86111d57997efe4034421751a163e100f0de5b8f
Your Name <you@example.com>
2019-08-13 15:37:03 -0500

    exercise #1

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,11 @@
 USE codeup_test_db;
-SHOW TABLES;
+
+CREATE TABLE albums (
+    'id' int UNSIGNED NOT NULL AUTO_INCREMENT,
+    'artist' varchar(50) NOT NULL,
+    'name' varchar(50) NOT NULL,
+    'release_date' int NOT NULL,
+    'sales' float NOT NULL,
+    'genre' varchar(50) NOT NULL,
+    PRIMARY KEY ('id')
+    );
