c601850b97404df50ac4d06c5c886fb7658810ec
Your Name <you@example.com>
2019-08-15 11:32:38 -0500

    finished exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,13 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE albums (
-    'id' int UNSIGNED NOT NULL AUTO_INCREMENT,
-    'artist' varchar(50) NOT NULL,
-    'name' varchar(50) NOT NULL,
-    'release_date' int NOT NULL,
-    'sales' float NOT NULL,
-    'genre' varchar(50) NOT NULL,
-    PRIMARY KEY ('id')
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    release_date int NOT NULL,
+    sales float NOT NULL,
+    genre varchar(100) NOT NULL,
+    PRIMARY KEY (id)
     );
---
+++ update_exercises.sql
@@ -0,0 +1,5 @@
+use codeup_test_db;
+
+SeLeCt 'All albums in your table.' as '';
+
+select 'All albums released before 1980' as '';
